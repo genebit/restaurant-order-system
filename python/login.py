74a4a30a42d128c5213413dc4962f2b6208a2160
genebit <genebitara@gmail.com>
2021-04-25 01:35:28 +0800

    Finished checking the fields, TODO: Finish the database checking and registering

--- python/login.py
+++ python/login.py
@@ -1,6 +1,13 @@
 import tkinter
+import sqlite3
 
 import loginpage
+
+username = 0
+password = 0
+
+user_exist = False
+empty = ""
 
 def back_to_login(thispanel, window):
     thispanel.pack_forget()
@@ -46,6 +53,7 @@ def display(prev, window):
     )
     username_text.place(x=250, y=110)
 
+    global username
     username = tkinter.Entry(
         window,
         width=30,
@@ -61,6 +69,7 @@ def display(prev, window):
     )
     password_text.place(x=250, y=190)
 
+    global password
     password = tkinter.Entry(
         window,
         width=30,
@@ -74,8 +83,41 @@ def display(prev, window):
         text="Login",
         font=("sans-serif", 12),
         bg="red",
-        width=20
+        width=20,
+        command=login
     )
     login_button.place(x=330, y=300)
 
-    
+def login():
+    if username.get() is not empty and password.get() is not empty:
+        # Check to the database if the user exist
+        if_user_exist(username.get(), password.get())
+        print(user_exist)
+        
+    else:
+        print("Missing field")
+
+def if_user_exist(name, password):
+    try:    
+        file_destination = "./database/users.db"
+
+        connection = None
+        connection = sqlite3.connect(file_destination)
+        
+        cur = connection.cursor()
+        cur.execute(f"SELECT Username, Password FROM Accounts WHERE Username='{name}' AND Password='{password}'")
+        
+        results = cur.fetchall()
+
+        global user_exist
+
+        # for items in results:
+        #     if items[0] is not []:
+        #         user_exist = True
+        #     else:
+        #         user_exist = False
+
+        connection.close()
+
+    except sqlite3.Error as err:
+        print(err)
