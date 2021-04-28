88dd3b72b1e87858ff3266cc780ff27e027159ec
genebit <genebitara@gmail.com>
2021-04-28 15:02:23 +0800

    Finished Setting button on menu, TODO: Code the onclick ev

--- python/main.py
+++ python/main.py
@@ -2,10 +2,11 @@ import tkinter
 import sqlite3
 from PIL import Image, ImageTk
 
+import menupage
+
 window = tkinter.Tk()
 
 empty = ""
-
 # Login fields
 l_img_path = "./img/login.png"
 l_img = ImageTk.PhotoImage(Image.open(l_img_path))
@@ -16,10 +17,15 @@ r_passwordInputField = None
 r_img_path = "./img/registration.png"
 r_img = ImageTk.PhotoImage(Image.open(r_img_path))
 
+# Menu page
+m_img_path = "./img/menupage.png"
+m_img = ImageTk.PhotoImage(Image.open(m_img_path))
+
 def login():
-    print("Logging user...")
+    print("* Logging user...")
+
     if l_usernameInputField.get() is not empty and l_passwordInputField.get() is not empty:
-        print("Fields are satisfied, checking user if exist...")
+        print("* Fields are satisfied, checking user if exist...")
 
         path = "./database/users.db"
         try:
@@ -27,23 +33,24 @@ def login():
             connection = sqlite3.connect(path)
             
             cur = connection.cursor()
-            cur = connection.execute(f"SELECT * FROM Accounts WHERE Username='{l_usernameInputField.get()}' AND Password='{l_passwordInputField.get()}';")
+            cur.execute(f"SELECT * FROM Accounts WHERE Username='{l_usernameInputField.get()}' AND Password='{l_passwordInputField.get()}';")
             results = cur.fetchone()
 
             if results != None:
-                print("Authentication satisfied. Sending to Menu page...")
+                print("* Authentication satisfied. Sending to Menu page...")
+                menupage.show(l_page, window, m_img)
             else:
-                print("User is not recorded! Please Register and try again.")
+                print("* User is not recorded! Please Register and try again.")
 
             connection.close()
 
         except sqlite3.Error as err:
             print(f"An error occured when 'Logging in', cause: {err}")
     else:
-        print("Please fill in the fields")
+        print("* Please fill in the fields")
 
 def show_register_page():
-    print("Registering...")
+    print("* Registering...")
 
     l_page.pack_forget()
 
@@ -79,10 +86,10 @@ def show_register_page():
     back_button.place(x=44, y=178)
 
 def register():
-    print("Checking into the database")
+    print("* Checking into the database")
 
     if r_usernameInputField.get() is not empty and r_passwordInputField.get() is not empty:
-        print("Fields are satisfied, checking user if exist...")
+        print("* Fields are satisfied, checking user if exist...")
 
         path = "./database/users.db"
         try:
@@ -94,30 +101,29 @@ def register():
             results = cur.fetchone()
 
             if results != None:
-                print("User is already recorded, Please use the login...")
+                print("* User is already recorded, Please use the login...")
             else:
-                print("Registering user...")
+                print("* Registering user...")
                 inserted_data = f"INSERT INTO Accounts(Username, Password)VALUES(?, ?);"
 
                 cur.execute(inserted_data, (r_usernameInputField.get(), r_usernameInputField.get()))
                 connection.commit()
-                print("Finished Registered user, Redirecting to Menu")
+                print("* Finished Registered user, Redirecting to Menu")
 
             connection.close()
             
         except sqlite3.Error as err:
             print(f"An error occured when 'Registering user', cause: {err}")
     else:
-        print("Please fill in the fields")
+        print("* Please fill in the fields")
 
 def back(panel):
-    print("Closing registration page")
+    print("* Closing registration page")
     
     panel.destroy()
     l_page.pack()
 
 try:
-
     print("Running...")
     
     # Frame
@@ -141,16 +147,15 @@ try:
         borderwidth=0, highlightthickness=0, width=9,
         bg="white", command=login
     )
-    login_button.place(x=410, y=358)
+    login_button.place(x=409, y=358)
 
     register_button = tkinter.Button(
         l_page, text="REGISTER", font=("Roboto", 18), 
         borderwidth=0, highlightthickness=0, width=9,
         bg="white", command=show_register_page
     )
-    register_button.place(x=576, y=358)
+    register_button.place(x=575, y=358)
     
-
     # Window Properties
     window.geometry("800x620")
     window.config(background="white")
--- python/menupage.py
+++ python/menupage.py
@@ -1,5 +1,50 @@
 import tkinter
-from PIL import ImageTk, Image
 
-def show(panel, window):
+def show(panel, window, img):
     print("Menu Page loaded.")
+
+    panel.pack_forget()
+
+    # Frame
+    menu_page = tkinter.Frame(window, width=800, height=620, bg="black")
+    menu_page.pack()
+
+    # Image Background
+    m_img_holder = tkinter.Label(menu_page, image=img)
+    m_img_holder.pack()
+
+    # Buttons
+    premium_set_order_button = tkinter.Button(
+        menu_page, text="Premium Set", font=("Roboto", 15), 
+        borderwidth=0, highlightthickness=0, width=10,
+        bg="white"
+    )
+    premium_set_order_button.place(x=62, y=210)
+
+    emperor_set_order_button = tkinter.Button(
+        menu_page, text="Emperor Set", font=("Roboto", 15), 
+        borderwidth=0, highlightthickness=0, width=10,
+        bg="white"
+    )
+    emperor_set_order_button.place(x=220, y=210)
+
+    delete_order_button = tkinter.Button(
+        menu_page, text="DELETE", font=("Roboto", 14), 
+        borderwidth=0, highlightthickness=0, width=5,
+        bg="white"
+    )
+    delete_order_button.place(x=430, y=510)
+
+    confirm_order_button = tkinter.Button(
+        menu_page, text="CONFIRM \nORDER", font=("Roboto", 12), 
+        borderwidth=0, highlightthickness=0, width=7, height=2,
+        bg="white"
+    )
+    confirm_order_button.place(x=540, y=500)
+
+    logout_button = tkinter.Button(
+        menu_page, text="LOGOUT", font=("Roboto", 14), 
+        borderwidth=0, highlightthickness=0, width=5,
+        bg="white"
+    )
+    logout_button.place(x=660, y=510)
