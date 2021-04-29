9ea49026a9f4f379f34483f5ace5b5c08ca8dd50
Gene Bitara <bitaragene@gmail.com>
2021-04-29 14:02:28 +0800

    Finished order calculator

--- python/order_calc.py
+++ python/order_calc.py
@@ -3,16 +3,18 @@ import tkinter
 root = tkinter.Tk()
 
 counter = tkinter.IntVar()
-
-# Can be ordered at top or at bottom, 
-# when on bottom, use command=lambda: function()
-# when on top, use command=function
+PRICE = 200
+total = tkinter.IntVar()
 
 def onClick():
     counter.set(counter.get() + 1)
-
+    
+    for item in range(counter.get()):
+        total.set("TOTAL: {}".format(counter.get() * PRICE))
+    
 tkinter.Label(root, textvariable=counter).pack()
 tkinter.Button(root, text="Increase", command=onClick).pack()
+tkinter.Label(root, textvariable=total).pack()
 
 root.geometry("200x200")
 root.mainloop()
