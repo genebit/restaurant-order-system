6316ba111ad2f798371f00aebb36c37a8128220e
Gene Bitara <bitaragene@gmail.com>
2021-04-29 21:47:11 +0800

    Update

--- python/menupage.py
+++ python/menupage.py
@@ -1,5 +1,17 @@
 import tkinter
 
+quantity = tkinter.IntVar()
+PREMIUM_SET_PRICE = 600
+EMPEROR_SET_PRICE = 800
+
+total = tkinter.IntVar()
+
+def premium_set_clicked():
+    counter.set(counter.get() + 1)
+    
+    for item in range(counter.get()):
+        total.set("TOTAL: {}".format(counter.get() * PRICE))
+
 def show(panel, window, img):
     print("Menu Page loaded.")
 
@@ -12,12 +24,27 @@ def show(panel, window, img):
     # Image Background
     m_img_holder = tkinter.Label(menu_page, image=img)
     m_img_holder.pack()
-
+    
+    # Static texts
+    premium_text = tkinter.Label(menu_page, text="PREMIUM SET", font=("Roboto", 13), bg="white")
+    emperor_text = tkinter.Label(menu_page, text="EMPEROR SET", font=("Roboto", 13), bg="white")
+    total_text = tkinter.Label(menu_page, text="TOTAL:", font=("Roboto", 15), bg="white")
+    
+    premium_text.place(x=440, y=310)
+    emperor_text.place(x=440, y=340)
+    total_text.place(x=440, y=400)
+    
+    p_quantity_text = tkinter.Label(menu_page, textvariable=quantity, font=("Roboto", 13))
+    p_quantity_text.place(x=400, y=200) 
+                                            
+    tkinter.Button(root, text="Increase", command=onClick).pack()
+    tkinter.Label(root, textvariable=total).pack()
+    
     # Buttons
     premium_set_order_button = tkinter.Button(
         menu_page, text="Premium Set", font=("Roboto", 15), 
         borderwidth=0, highlightthickness=0, width=10,
-        bg="white"
+        bg="white", command=premium_set_clicked
     )
     premium_set_order_button.place(x=62, y=210)
 
