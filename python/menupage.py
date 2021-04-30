c88478333296e10274eb761b4c397e505749035b
genebit <genebitara@gmail.com>
2021-04-30 20:08:59 +0800

    Fixed button layout in menupage.py

--- python/menupage.py
+++ python/menupage.py
@@ -43,35 +43,35 @@ def show(panel, window, img):
     # Buttons
     premium_set_order_button = tkinter.Button(
         menu_page, text="Premium Set", font=("Roboto", 15), 
-        borderwidth=5, highlightthickness=0, width=10, height=1,
+        borderwidth=0, highlightthickness=0, width=10, height=1,
         bg="white", #command=premium_set_clicked
     )
     premium_set_order_button.place(x=70, y=210)
 
-    # emperor_set_order_button = tkinter.Button(
-    #     menu_page, text="Emperor Set", font=("Roboto", 15), 
-    #     borderwidth=0, highlightthickness=0, width=10,
-    #     bg="white"
-    # )
-    # emperor_set_order_button.place(x=220, y=210)
+    emperor_set_order_button = tkinter.Button(
+        menu_page, text="Emperor Set", font=("Roboto", 15), 
+        borderwidth=0, highlightthickness=0, width=10,
+        bg="white"
+    )
+    emperor_set_order_button.place(x=227, y=210)
 
-    # delete_order_button = tkinter.Button(
-    #     menu_page, text="DELETE", font=("Roboto", 14), 
-    #     borderwidth=0, highlightthickness=0, width=5,
-    #     bg="white"
-    # )
-    # delete_order_button.place(x=430, y=510)
+    delete_order_button = tkinter.Button(
+        menu_page, text="DELETE", font=("Roboto", 14), 
+        borderwidth=0, highlightthickness=0, width=6,
+        bg="white"
+    )
+    delete_order_button.place(x=438, y=515)
 
-    # confirm_order_button = tkinter.Button(
-    #     menu_page, text="CONFIRM \nORDER", font=("Roboto", 12), 
-    #     borderwidth=0, highlightthickness=0, width=7, height=2,
-    #     bg="white"
-    # )
-    # confirm_order_button.place(x=540, y=500)
+    confirm_order_button = tkinter.Button(
+        menu_page, text="CONFIRM \nORDER", font=("Roboto", 13), 
+        borderwidth=0, highlightthickness=0, width=9, height=2,
+        bg="white"
+    )
+    confirm_order_button.place(x=540, y=500)
 
-    # logout_button = tkinter.Button(
-    #     menu_page, text="LOGOUT", font=("Roboto", 14), 
-    #     borderwidth=0, highlightthickness=0, width=5,
-    #     bg="white"
-    # )
-    # logout_button.place(x=660, y=510)
+    logout_button = tkinter.Button(
+        menu_page, text="LOGOUT", font=("Roboto", 14), 
+        borderwidth=0, highlightthickness=0, width=6,
+        bg="white"
+    )
+    logout_button.place(x=663, y=515)
